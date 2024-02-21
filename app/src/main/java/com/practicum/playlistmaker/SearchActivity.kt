5fd57d7b544edf73c0ee44eebcd1c670697ed924
sysooevAndrey <sysoyev.and@gmail.com>
2024-02-21 19:19:12 +0300

    12 commit

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,60 +1,174 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.ImageView
-import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@Suppress("UNUSED_EXPRESSION")
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        private const val SEARCH_TEXT_DEF: String = ""
+        private const val BASE_URL: String = "https://itunes.apple.com"
+    }
 
     private val trackList = ArrayList<TrackResponse.Track>()
 
-    private var searchText: String = Search.SEARCH_TEXT_DEF
-
-    private val baseUrl: String = "https://itunes.apple.com"
+    private var searchText: String = SEARCH_TEXT_DEF
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl(baseUrl)
+        .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     private val iTunesSearchService: ITunesApi =
         retrofit.create(ITunesApi::class.java)
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val placeholderEmpty = findViewById<LinearLayout>(R.id.placeholder_empty)
+        val placeholderSearch = findViewById<FrameLayout>(R.id.placeholder_view)
 
-        val placeholderNetwork = findViewById<LinearLayout>(R.id.placeholder_network)
+        val placeholderSearchImageView = findViewById<ImageView>(R.id.placeholder_image)
+
+        val placeholderSearchTextView = findViewById<TextView>(R.id.placeholder_text)
 
         val updateButton = findViewById<MaterialButton>(R.id.update_button)
 
-        val trackAdapter = TrackAdapter(trackList)
+        val clearButton = findViewById<ImageView>(R.id.clear_text)
+
+        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
 
         NavigationButton.back<ImageView>(this, R.id.back)
-        val searchEditText =
-            Search.search(
-                this,
-                R.id.search_edit_text,
-                searchText,
-                iTunesSearchService,
-                trackList,
-                trackAdapter,
-                placeholderEmpty,
-                placeholderNetwork,
-                updateButton
-            )
-        searchText = searchEditText.text.toString()
 
-        val searchRecyclerView =
-            findViewById<RecyclerView>(R.id.search_recycler_view)
+        val trackAdapter = TrackAdapter(trackList)
+
+        searchEditText.setText(searchText)
+
+        fun placeholderVisibility(searchStatus: SearchStatus) {
+            when (searchStatus) {
+                SearchStatus.SUCCESS -> {
+                    placeholderSearch.isVisible = false
+                }
+
+                SearchStatus.NOT_FOUND -> {
+                    placeholderSearchImageView.setImageResource(R.drawable.placeholder_empty)
+                    placeholderSearchTextView.setText(R.string.empty_text)
+                    placeholderSearch.isVisible = true
+                    updateButton.isVisible = false
+                }
+
+                SearchStatus.NO_NETWORK -> {
+                    placeholderSearchImageView.setImageResource(R.drawable.placeholder_network)
+                    placeholderSearchTextView.setText(R.string.network_error_text)
+                    placeholderSearch.isVisible = true
+                    updateButton.isVisible = true
+                }
+            }
+        }
+
+        fun createRequest() {
+            trackList.clear()
+            placeholderSearch.isVisible = false
+            iTunesSearchService
+                .search(searchText)
+                .enqueue(object : Callback<TrackResponse> {
+                    @SuppressLint("NotifyDataSetChanged")
+                    override fun onResponse(
+                        call: Call<TrackResponse>,
+                        response: Response<TrackResponse>
+                    ) {
+                        if (response.code() == 200) {
+                            trackList.addAll(response.body()?.results!!)
+                            trackAdapter.notifyDataSetChanged()
+                            if (trackList.isEmpty()) {
+                                placeholderVisibility(SearchStatus.NOT_FOUND)
+                            }
+                        } else {
+                            placeholderVisibility(SearchStatus.NO_NETWORK)
+                        }
+                    }
+
+                    override fun onFailure(
+                        call: Call<TrackResponse>,
+                        t: Throwable
+                    ) {
+                        placeholderVisibility(SearchStatus.NO_NETWORK)
+                    }
+                })
+            true
+        }
+
+        updateButton.setOnClickListener() {
+            createRequest()
+        }
+
+        clearButton.setOnClickListener {
+            searchEditText.setText(SEARCH_TEXT_DEF)
+            trackList.clear()
+            trackAdapter.notifyDataSetChanged()
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager
+                ?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        }
+
+        val searchEditTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                clearButton.isVisible = clearButtonVisibility(s)
+                searchText = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+
+        searchEditText.addTextChangedListener(searchEditTextWatcher)
+        searchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                createRequest()
+            }
+            false
+        }
+
+        val searchRecyclerView = findViewById<RecyclerView>(R.id.search_recycler_view)
 
         searchRecyclerView.adapter = trackAdapter
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Boolean {
+        return !s.isNullOrEmpty()
+    }
 }
+
---
+++ app/src/main/java/com/practicum/playlistmaker/SearchStatus.kt
@@ -0,0 +1,7 @@
+package com.practicum.playlistmaker
+
+enum class SearchStatus {
+    SUCCESS,
+    NOT_FOUND,
+    NO_NETWORK,
+}
